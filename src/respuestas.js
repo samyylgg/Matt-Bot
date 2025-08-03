ddf65d3c6a41141c80d674d5b6613b04fdb7fde4
samyylgg <liviuwu5@gmail.com>
2025-08-03 16:28:57 -0600

    Create respuestas.js

---
+++ src/respuestas.js
@@ -0,0 +1,9 @@
+export const respuestas = {
+  que: ['so 🧀', 'so un queso 🧀', 'soooo 🧀'],
+  uwu: ['uwu 💖', 'uwu, qué lindo 💖', 'uwu uwu uwu 💖'],
+  'quién eres': ['Soy Matt-Bot 🤖 by Sam', 'El bot más cool, Matt-Bot 🤖', 'Aquí Matt-Bot para servirte 🤖'],
+  hola: ['Hola! ¿En qué puedo ayudarte?', '¡Hey! ¿Qué tal?'],
+  'buenos días': ['¡Buenos días! ☀️', '¡Que tengas un excelente día! 😊'],
+  'buenas noches': ['¡Buenas noches! 🌙', '¡Que descanses! 😴'],
+  gracias: ['De nada 😊', '¡A la orden! 🤖', '¡Por nada!'],
+};
