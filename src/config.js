19560f3ba84b143b43b2ae88550e76f73b34a82f
samyylgg <liviuwu5@gmail.com>
2025-08-03 16:40:24 -0600

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  nombreBot: 'Matt-Bot',
+  autor: 'by Sam',
+  prefijo: '.',
+  emoji: '🤖',
+  mensajeBienvenida: '¡Hola! Soy Matt-Bot, listo para ayudarte.',
+  mensajeDespedida: 'Hasta luego. ¡Que tengas buen día!',
+}
