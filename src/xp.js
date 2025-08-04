585357eb815c4a7224a8143300d1f978af24b884
Sam <liviuwu5@gmail.com>
2025-08-04 10:03:57 -0600

    Crear xp.js

---
+++ src/xp.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+const path = './data/xp.json';
+
+// Asegura que el archivo exista
+if (!fs.existsSync(path)) fs.writeFileSync(path, '{}');
+
+function getXP(user) {
+  const data = JSON.parse(fs.readFileSync(path));
+  return data[user] || 0;
+}
+
+function addXP(user, amount) {
+  const data = JSON.parse(fs.readFileSync(path));
+  data[user] = (data[user] || 0) + amount;
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+export { getXP, addXP };
